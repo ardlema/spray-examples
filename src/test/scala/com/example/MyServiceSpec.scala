222883b0795e33c6c72088c20e24032ac49b4568
Alberto <albertorodriguez@paradigmatecnologico.com>
2014-02-26 16:47:37 +0100

    Fixed tests

--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -5,25 +5,37 @@ import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
-class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
+class MyServiceSpec
+  extends Specification
+  with Specs2RouteTest
+  with ServiceHello
+  with ServiceGoodBye {
   def actorRefFactory = system
   
   "MyService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
-        responseAs[String] must contain("Say hello")
+    "return a greeting for GET requests to the hello path" in {
+      Get("/hello") ~> helloRoute ~> check {
+        val responseAsString = responseAs[String]
+        responseAsString must contain("Say hello")
+      }
+    }
+
+    "return a greeting for GET requests to the bye path" in {
+      Get("/bye") ~> goodByeRoute ~> check {
+        val responseAsString = responseAs[String]
+        responseAsString must contain("Say goodbye")
       }
     }
 
     "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
+      Get("/kermit") ~> helloRoute ~> check {
         handled must beFalse
       }
     }
 
-    "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
+    "return a MethodNotAllowed error for PUT requests to the hello path" in {
+      Put("/hello") ~> sealRoute(helloRoute) ~> check {
         status === MethodNotAllowed
         responseAs[String] === "HTTP method not allowed, supported methods: GET"
       }
